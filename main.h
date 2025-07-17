e61019b641721f0b0cae0758f07e1c13a49b1eb7
Jrizzo123 <77479285+Jrizzo123@users.noreply.github.com>
2025-07-17 03:23:21 -0500

    Create main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef BINTREE_MAIN_H
+#define BINTREE_MAIN_H
+
+#include <iostream>
+#include "bintree.h"
+#include "functions.h"
+
+#define TESTDATA1 7
+#define TESTDATA2 6
+
+using std::cout;
+using std::endl;
+
+#endif /* BINTREE_MAIN_H */
